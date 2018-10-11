1581e6d580c6b7bbbfc4ee8b840bcb41b0358a23
Si1mT <33309403+Si1mT@users.noreply.github.com>
2018-10-11 09:04:17 +0300

    initial commit

---
+++ ListViewExample/ListViewExample/CustomAdapter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ListViewExample
+{
+    public class CustomAdapter : BaseAdapter<string>
+    {
+        string[] items;
+        Activity context;
+
+        public CustomAdapter(Activity context, string[] items) : base()
+        {
+            this.context = context;
+            this.items = items;
+        }
+
+        public override string this[int position]
+        {
+            get {return items[position]; }
+        }
+        //public override string this[int position] => throw new NotImplementedException();
+
+        public override int Count => throw new NotImplementedException();
+
+        public override long GetItemId(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ListViewExample/ListViewExample/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace ListViewExample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : ListActivity
+    {
+        string[] countries = new string[] { "estonia", "romania", "france" };
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.activity_main);
+            ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleExpandableListItem1, countries);
+
+
+        }
+    }
+}
